582b144f7baf48db96e07882041b003c1ce883e2
OlgaPython71 <kuznetsova-o@mail.ru>
2024-10-01 15:47:20 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,18 +1,14 @@
 def custom_write(file_name, strings):
     number_strings = 0 # счетчик строк
-    list_text = [] # список для текста из info
-    list_number = [] # список для кортежей с номером строки и байта
+    strings_positions = {}
+    file = open(file_name, 'a', encoding='utf-8')  # открыли файл
     for i in strings: # цикл для перебора элементов strings
-        file = open(file_name, 'a', encoding='utf-8')  # открыли файл
         byte_beginning_string = file.tell()
-        list_text.append(i)
-        file.write(f'{i}\n')
         number_strings += 1
-        tuple_ = (number_strings,) + (byte_beginning_string,)
-        list_number.append(tuple_)
-        strings_positions = dict(zip(list_number, list_text))
-        file.close()
+        strings_positions.update({((number_strings,) + (byte_beginning_string,)): i})
+        file.write(f'{i}\n')
     return strings_positions
+    file.close()
 
 
 info = [
