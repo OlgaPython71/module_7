e4d3d70d045fc511f962df717e060a30746438b5
OlgaPython71 <kuznetsova-o@mail.ru>
2024-10-03 19:57:13 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,14 +1,14 @@
 def custom_write(file_name, strings):
-    number_strings = 0 # счетчик строк
+    number_strings = 0  # счетчик строк
     strings_positions = {}
-    file = open(file_name, 'a', encoding='utf-8')  # открыли файл
-    for i in strings: # цикл для перебора элементов strings
+    file = open(file_name, 'w', encoding='utf-8')  # открыли файл
+    for i in strings:  # цикл для перебора элементов strings
         byte_beginning_string = file.tell()
         number_strings += 1
         strings_positions.update({((number_strings,) + (byte_beginning_string,)): i})
         file.write(f'{i}\n')
-    return strings_positions
     file.close()
+    return strings_positions
 
 
 info = [
@@ -16,7 +16,7 @@ info = [
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-        ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
